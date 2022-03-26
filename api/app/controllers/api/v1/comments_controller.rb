3de08bf74c699cd25b2d904912c369d99065f0ff
qunitaros <kunitaros8585@icloud.com>
2022-03-26 21:57:07 +0900

    [add]comment_controllerの記述

--- api/app/controllers/api/v1/comments_controller.rb
+++ api/app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::CommentsController < ApplicationController
+  def create
+    comment = Comment.new(comment_params)
+    
+    if comment.save
+      render json: { status: 200, comment: comment }
+    else
+      render json: { status: 500, comment: "作成に失敗しました。" }
+    end
+  end
+
+  private
+
+    def comment_params
+      params.permit(:post_id, :user_id, :comment)
+    end
 end
