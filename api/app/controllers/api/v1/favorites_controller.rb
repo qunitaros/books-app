79fb8100a47182ea5dce79dd72c617249117893a
qunitaros <kunitaros8585@icloud.com>
2022-03-26 23:05:21 +0900

    [add]favorites_controllerの記述

--- api/app/controllers/api/v1/favorites_controller.rb
+++ api/app/controllers/api/v1/favorites_controller.rb
@@ -1,2 +1,29 @@
 class Api::V1::FavoritesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    favorites = Favorite.filter_by_post(params[:post_id]).select(:id, :user_id, :post_id)
+    render json: { status: 200, favorites: favorites }
+  end
+
+  def create
+    favorite = Favorite.new(favorite_params)
+    if favorite.save
+      render json: { status: 200, favorite: favorite }
+    else
+      render json: { status: 500, message: "作成に失敗しました。" }
+    end
+  end
+
+  def destroy
+    @favorite = current_api_v1_user.favorites.find(params[:id])
+    @favorite.destroy
+    render json: { status: 200, favorite: @favorite }
+  end
+
+  private
+
+  def favorite_params
+    params.permit(:post_id, :user_id)
+  end
 end
--- api/app/models/favorite.rb
+++ api/app/models/favorite.rb
@@ -5,4 +5,7 @@ class Favorite < ApplicationRecord
   validates_uniqueness_of :post_id, scope: :user_id
   validates :post_id, presence: true
   validates :user_id, presence: true
+
+  scope :filter_by_post, ->(post_id) { where(post_id: post_id) if post_id }
+end
 end
